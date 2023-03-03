7df02623b392c24348486dcb4f78ae39c0d9f4a6
rushabh9430 <sutariyarushabh2@gmail.com>
2023-03-03 12:22:10 +0530

    copmmit 5

---
+++ app/src/main/java/com/example/resume2/Resume6.java
@@ -0,0 +1,14 @@
+package com.example.resume2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Resume6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resume6);
+    }
+}
---
+++ app/src/main/java/com/example/resume2/Resume7_My_Project.java
@@ -0,0 +1,14 @@
+package com.example.resume2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Resume7_My_Project extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resume7_my_project);
+    }
+}
